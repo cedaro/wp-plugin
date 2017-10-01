b4d0e305a9c8c5c6d831d94771b3e951bd4b3051
Brady Vercher <brady@blazersix.com>
2017-10-01 07:50:50 -0700

    Introduce a base hook provider class.

---
+++ src/AbstractHookProvider.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Base hook provider.
+ *
+ * @package   Cedaro\WP\Plugin
+ * @copyright Copyright (c) 2017 Cedaro, LLC
+ * @license   MIT
+ */
+
+namespace Cedaro\WP\Plugin;
+
+use Cedaro\WP\Plugin\HookProviderInterface;
+use Cedaro\WP\Plugin\HooksTrait;
+use Cedaro\WP\Plugin\PluginAwareInterface;
+use Cedaro\WP\Plugin\PluginAwareTrait;
+
+/**
+ * Base hook provider class.
+ *
+ * @package Cedaro\WP\Plugin
+ */
+abstract class AbstractHookProvider implements HookProviderInterface, PluginAwareInterface {
+
+	use HooksTrait, PluginAwareTrait;
+
+	/**
+	 * Registers hooks for the plugin.
+	 */
+	abstract public function register_hooks();
+}

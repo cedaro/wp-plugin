52cab888011918c4a91e8aa18f2233630539da92
Brady Vercher <brady@blazersix.com>
2017-12-21 14:40:32 -0800

    Introduce a factory to easily create new plugin instances.

---
+++ src/PluginFactory.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Plugin factory.
+ *
+ * @package   Cedaro\WP\Plugin
+ * @copyright Copyright (c) 2017 Cedaro, LLC
+ * @license   MIT
+ */
+
+namespace Cedaro\WP\Plugin;
+
+use Cedaro\WP\Plugin\Plugin;
+
+/**
+ * Plugin factory class.
+ *
+ * @package Cedaro\WP\Plugin
+ */
+class PluginFactory {
+	/**
+	 * Create a plugin instance.
+	 *
+	 * @param string $slug     Plugin slug.
+	 * @param string $filename Optional. Absolute path to the main plugin file.
+	 *                         This should be passed if the calling file is not
+	 *                         the main plugin file.
+	 * @return Plugin Plugin instance.
+	 */
+	public static function create( $slug, $filename = '' ) {
+		// Use the calling file as the main plugin file.
+		if ( empty( $filename ) ) {
+			$backtrace = debug_backtrace( DEBUG_BACKTRACE_PROVIDE_OBJECT, 1 );
+			$filename = $backtrace[0]['file'];
+		}
+
+		return ( new Plugin() )
+			->set_basename( plugin_basename( $filename ) )
+			->set_directory( plugin_dir_path( $filename ) )
+			->set_file( $filename )
+			->set_slug( $slug )
+			->set_url( plugin_dir_url( $filename ) )
+			->set_container( new Container() );
+	}
+}
